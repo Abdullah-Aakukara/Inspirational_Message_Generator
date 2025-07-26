434a0469ccb0bd3480199f28bef6c28c63f926ed
Abdullah-Aakukara <abdullahaakukara@gmail.com>
2025-07-26 16:38:03 +0530

    complete working program!

---
+++ data.js
@@ -0,0 +1,7 @@
+    objectStrings = ['I', 'Me', 'You', 'We', 'They', 'She', 'He']
+    verbStrings =  ['can', 'will', 'must', 'shall', 'can\'t', 'willn\'t'] 
+    subjectStrings =  ['CRACK-IT', 'SQWEEZE-IT', 'F*CK-IT']
+
+module.exports.objectStrings = objectStrings;
+module.exports.verbStrings = verbStrings;
+module.exports.subjectStrings = subjectStrings;
--- randomMessages.js
+++ randomMessages.js
@@ -1,14 +1,24 @@
+const objectOfstrings = require('./data.js')
 console.log("Hello! \nHere's your today's Message!");
 // function to randomly return subject
-function selectSubject(arr) {
+function returnSubject(arr) {
      let index = Math.floor(Math.random() * arr.length)
      return arr[index];
 }
 // function to randomly return verbs
-function selectVerb(arr) {
+function returnVerb(arr) {
     let index = Math.floor(Math.random()*arr.length);
     return arr[index];
 }
-const displayMessage = (fun1, fun2) => {
-    console.log("You " + fun2() + fun1() + ' !');
+// function to randomly return objects
+function returnObject(arr) {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
 }
+const displayMessage = (fun1, fun2, fun3) => {
+    console.log(fun1(objectOfstrings.objectStrings) + ' ' + fun2(objectOfstrings.verbStrings) + ' ' + fun3(objectOfstrings.subjectStrings) + ' !');
+}
+
+displayMessage(returnObject, returnVerb, returnSubject);
+console.log(objectOfstrings)
+
